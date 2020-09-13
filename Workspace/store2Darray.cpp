a88a47ac4d70639dafde734c139755ac111e7c71
Ravi Kumar <ravikuk@outlook.com>
2020-09-13 21:59:22 +0530

    Day 1
    Coding

---
+++ Workspace/store2Darray.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <vector>
+using std::vector;
+using std::cout;
+
+int main()
+{
+    vector<vector<int>> board = {{0, 1, 0, 0, 0, 0},
+{0, 1, 0, 0, 0, 0},
+{0, 1, 0, 0, 0, 0},
+{0, 1, 0, 0, 0, 0},
+{0, 0, 0, 0, 1, 0}};
+
+}
